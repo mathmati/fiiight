368129d9ad1e11b3a0528520d9854e6a0e5620c6
Lint Action <lint-action@samuelmeuli.com>
2026-06-05 01:08:05 +0000

    style: fix code style issues with gofmt

--- src/iniutils.go
+++ src/iniutils.go
@@ -2846,7 +2846,7 @@ func parseMusicSection(section *ini.Section) Music {
 
 	// Parse keys
 	for _, key := range section.Keys() {
-		rawKey := strings.TrimSpace(key.Name())  // e.g. "title.bgm.loop" or "bgmusic"
+		rawKey := strings.TrimSpace(key.Name())                // e.g. "title.bgm.loop" or "bgmusic"
 		rawVal := strings.TrimSpace(StripComment(key.Value())) // e.g. "1, 2, 3"
 		if rawKey == "" {
 			continue
